4aacb6fc98b81e1efa1ba7d7278b216cf9697bca
Oscar Hugo Paz <oscarhugopazmariscal@gmail.com>
2025-01-05 00:38:22 -0500

    Add Fathom script

--- index.php
+++ index.php
@@ -442,6 +442,7 @@ run();
     <link href="https://cdn.jsdelivr.net/npm/vuetify@v3.7.6/dist/vuetify.min.css" rel="stylesheet">
     <link rel="icon" href="favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="YXQSEBLN" defer></script>
     <style>
     [v-cloak] > * {
         display:none;
