6ea88397331d629e5a034aa2f3aba2756dff819b
Oscar Hugo Paz <oscarhugopazmariscal@gmail.com>
2025-05-05 15:05:37 -0500

    Add analytics scripts

--- index.php
+++ index.php
@@ -427,8 +427,16 @@ run();
     <link href="https://cdn.jsdelivr.net/npm/vuetify@v3.7.6/dist/vuetify.min.css" rel="stylesheet">
     <link rel="icon" href="favicon.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-    <!-- Fathom Analytics (opcional) -->
+    <!-- Fathom Analytics -->
     <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="YXQSEBLN" defer></script>
+    <!-- Microsoft Clarity -->
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "joxkwqobqo");
+    </script>
     <style>
     [v-cloak] > * {
         display:none;
